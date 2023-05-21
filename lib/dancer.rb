5908e303ea48a303404f6ac4563e5a4a08660477
mark mwendwa <mwendwamark310@gmail.com>
2023-05-21 14:56:47 +0300

    Done

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,14 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+class Dancer 
+    extend MetaDancing
+    include Dance
+    attr_accessor :name
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+    def initialize(name)
+        @name = name
+    end
+end
